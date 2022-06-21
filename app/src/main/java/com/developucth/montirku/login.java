dc21239a992b21c5551b76a783e9d290cf3d48ef
yogga <yoga_pratama@gmail.com>
2022-06-21 10:03:12 -0700

    fk

--- app/src/main/java/com/developucth/montirku/login.java
+++ app/src/main/java/com/developucth/montirku/login.java
@@ -3,12 +3,27 @@ package com.developucth.montirku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class login extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
